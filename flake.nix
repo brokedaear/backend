84bdf0421cd7e92c419155e6a474ed7609fe89fb
Neo <99268950+n30w@users.noreply.github.com>
2025-06-08 18:59:34 -1000

    fix: remove `treefmt` for now
    This causes issues that I don't understand yet. Also, I used this repo
    for inspiration for changing system impls
    https://github.com/NixOS/templates/blob/master/go-hello/flake.nix

--- flake.nix
+++ flake.nix
@@ -8,18 +8,23 @@
     systems.url = "systems";
 
     # Code QL
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
+    # treefmt-nix = {
+    #   url = "github:numtide/treefmt-nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    pre-commit-hooks = { 
+      url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      treefmt-nix,
+      # treefmt-nix,
       systems,
       ...
     }@inputs:
@@ -30,66 +35,69 @@
         };
       };
 
-      commonPackages = with pkgs; [
-        # Go related
-        go # Need that obviously
-        gofumpt # Go formatter
-        golangci-lint # Local/CI linter
-        gopls
-        gotestsum # Pretty tester
-        gotools
-
-        # Library related
-        stripe-cli # Stripe integration
-        upx # Binary shrinker
-
-        # Dev tools
-        openapi-generator-cli
-        jq # JSON manipulation
-        yq # YAML manipulation
-        tokei # CLOC
-        reuse # LICENSE compliance
-
-        # Formatting
-        nixfmt-rfc-style
-
-        # System tools
-        lazygit # TUI Git interface
-        mprocs # Process runner
-        neovim # Better vim
-        helix # Quick text editor
-        go-task # Makefile alternative
-        vegeta # HTTP Load Testing Tool
-        figlet # Terminal text ASCII
-      ];
-
-      eachSystem =
-        f:
-        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
-      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+      nixpkgsFor = eachSystem (system: import nixpkgs { inherit system; });
+      # treefmtEval = eachSystem (system: treefmt-nix.lib.evalModule system ./treefmt.nix);
     in
     {
-      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      # formatter = eachSystem (system: treefmtEval.${pkgs.system}.config.build.wrapper);
 
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShellNoCC {
-          # packages = commonPackages;
-          buildInputs = [ ] ++ commonPackages;
+      devShells = eachSystem (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          commonPackages = with pkgs; [
+            # Go related
+            go # Need that obviously
+            gofumpt # Go formatter
+            golangci-lint # Local/CI linter
+            gopls
+            gotestsum # Pretty tester
+            gotools
 
-          REUSE_COPYRIGHT = "BROKE DA EAR LLC <https://brokedaear.com>";
-          REUSE_LICENSE = "Apache-2.0";
+            # Library related
+            stripe-cli # Stripe integration
+            upx # Binary shrinker
 
-          shellHook = ''
-            # eval "$(starship init bash)"
-            export PS1='$(printf "\033[01;34m(nix) \033[00m\033[01;32m[%s] \033[01;33m\033[00m$\033[00m " "\W")'
-          '';
-        };
+            # Dev tools
+            openapi-generator-cli
+            jq # JSON manipulation
+            yq # YAML manipulation
+            tokei # CLOC
+            reuse # LICENSE compliance
+
+            # Formatting
+            nixfmt-rfc-style
+
+            # System tools
+            lazygit # TUI Git interface
+            mprocs # Process runner
+            neovim # Better vim
+            helix # Quick text editor
+            go-task # Makefile alternative
+            vegeta # HTTP Load Testing Tool
+            figlet # Terminal text ASCII
+          ];
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            # packages = commonPackages;
+            buildInputs = [ ] ++ commonPackages;
+            
+            # Environment variables
+            REUSE_COPYRIGHT = "BROKE DA EAR LLC <https://brokedaear.com>";
+            REUSE_LICENSE = "Apache-2.0";
+
+            shellHook = ''
+              # eval "$(starship init bash)"
+              export PS1='$(printf "\033[01;34m(nix) \033[00m\033[01;32m[%s] \033[01;33m\033[00m$\033[00m " "\W")'
+            '';
+          };
       });
 
       checks = eachSystem (pkgs: {
         # Throws an error if any of the source files are not correctly formatted
         # when you run `nix flake check --print-build-logs`. Useful for CI
-        treefmt = treefmtEval.${pkgs.system}.config.build.check self;
+        # treefmt = treefmtEval.${pkgs.system}.config.build.check self;
       });
     };
 }
