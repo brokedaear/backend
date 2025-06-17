2449602fad24b18b8a3f9cc5df41ed541bc1d637
Neo <99268950+n30w@users.noreply.github.com>
2025-06-16 19:53:06 -1000

    chore: add licenses to loggers files

--- internal/common/utils/loggers/loggers.go
+++ internal/common/utils/loggers/loggers.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 BROKE DA EAR LLC <https://brokedaear.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package loggers
 
 // logger describes a custom logging implementation. For example, one might
